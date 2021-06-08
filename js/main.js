a0ef2b2e1059e631612f5cba8d40bb78506b1389
elgrando3000 <elgrando3000@gmail.com>
2021-06-08 14:15:42 +0300

    Create: added jquery validator

--- js/main.js
+++ js/main.js
@@ -56,4 +56,23 @@ menuButton.on("click", function (){
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  // Обработка форм
+  $('.form').each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Укажите Ваше имя",
+          minlength: "Имя не должно быть короче 2 букв",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+      },
+        phone: {
+          required: "Телефон обязателен"
+        },
+      },
+    });
+  });
 });
